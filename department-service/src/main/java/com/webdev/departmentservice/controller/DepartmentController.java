9cb60f3b05ed2eb0892667f0a8599468b65f923b
Sultan <osultan@yandex.ru>
2024-12-29 22:47:47 +0500

    Add DepartmentController and createDepartment endpoint

---
+++ department-service/src/main/java/com/webdev/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,24 @@
+package com.webdev.departmentservice.controller;
+
+import com.webdev.departmentservice.dto.DepartmentDto;
+import com.webdev.departmentservice.service.DepartmentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/departments")
+@AllArgsConstructor
+public class DepartmentController {
+    private DepartmentService departmentService;
+
+    @PostMapping
+    public ResponseEntity<DepartmentDto> createDepartment(@RequestBody DepartmentDto departmentDto) {
+        DepartmentDto department = departmentService.addDepartment(departmentDto);
+        return new ResponseEntity<>(department, HttpStatus.CREATED);
+    }
+}
